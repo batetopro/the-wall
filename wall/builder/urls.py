cac38cd60e285b829c51de04d92b1f0cd23371da
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2023-02-13 13:37:52 +0200

    Add swagger.

--- wall/builder/urls.py
+++ wall/builder/urls.py
@@ -5,11 +5,11 @@ from . import views
 
 app_name = 'builder'
 urlpatterns = [
-    path('profiles/<int:profile>/days/<int:day>/',
+    path('<int:profile>/days/<int:day>/',
          views.ProfileIceAmountPerDay.as_view(), name='amount_per_profile_per_day'),
-    path('profiles/<int:profile>/overview/<int:day>/',
+    path('<int:profile>/overview/<int:day>/',
          views.ProfilePricePerDay.as_view(), name='price_per_profile_per_day'),
-    path('profiles/overview/<int:day>/',
+    path('overview/<int:day>/',
          views.PricePerDay.as_view(), name='price_per_day'),
-    path('profiles/overview/', views.PriceTotal.as_view(), name='overall'),
+    path('overview/', views.PriceTotal.as_view(), name='overall'),
 ]
